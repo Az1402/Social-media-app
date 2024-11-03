f4a992f14c4213548f4d2fa80fca82a06d1b8d0b
Az1402 <atharvrzalte@gmail.com>
2024-11-03 16:37:32 +0530

    update in users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,6 @@
+// routes/users.js
 import express from "express";
-import {
-  getUser,
-  getUserFriends,
-  addRemoveFriend,
-} from "../controllers/users.js";
+import { getUser, getUserFriends, addRemoveFriend } from "../controllers/Users.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
